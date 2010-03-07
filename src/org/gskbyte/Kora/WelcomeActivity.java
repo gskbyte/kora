035781178bbc3b31dafc329bde6f975844223f80
alexvisio <alexvisio>
2010-03-07 17:30:19 +0000

    Hemos añadido funcionalidad al boton de informacion de la pantalla de bienvenida. Novedades: Declarados strings para autor, version y licencia. Incluido un metodo onCreateDialog en la actividad Welcome y algunos miembros nuevos.

--- src/org/gskbyte/Kora/WelcomeActivity.java
+++ src/org/gskbyte/Kora/WelcomeActivity.java
@@ -5,6 +5,9 @@ import org.gskbyte.Kora.R;
 import org.gskbyte.Kora.Settings.*;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -16,8 +19,10 @@ import android.widget.TextView;
 public class WelcomeActivity extends Activity
 {
     private static final String TAG = "WelcomeActivity";
+
+    static final int ID_DIALOG_INFO = 1;
     
-    private Button startButton, settingsButton;
+    private Button startButton, settingsButton, infoButton;
     private TextView autostartText;
     
     private void init()
@@ -32,11 +37,13 @@ public class WelcomeActivity extends Activity
         
         startButton = (Button)findViewById(R.id.startButton);
         settingsButton = (Button)findViewById(R.id.settingsButton);
+        infoButton = (Button) findViewById(R.id.infoButton);
         autostartText =  (TextView)findViewById(R.id.autostart);
         
         // Asociar eventos con botones
         startButton.setOnClickListener(startButtonListener);
         settingsButton.setOnClickListener(settingsButtonListener);
+        infoButton.setOnClickListener(infoButtonListener);
         
         // Cargar datos de programa (usuarios, perfiles, etc)
         init();
@@ -56,6 +63,38 @@ public class WelcomeActivity extends Activity
         
     }
     
+    protected Dialog onCreateDialog(int id)
+    {
+        Dialog dialog;
+        switch(id)
+        {
+        case ID_DIALOG_INFO:
+        	String appName = getResources().getString(R.string.appName),
+        	ok = getResources().getString(R.string.ok),
+        	version = getResources().getString(R.string.version) + ": " +
+        	          getResources().getString(R.string.version_value),
+        	author = getResources().getString(R.string.author) + ": " +
+        	         getResources().getString(R.string.author_value),
+        	license = getResources().getString(R.string.license);
+        	
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage(version + "\n" + author + "\n" + license)
+                   .setIcon(getResources().getDrawable(R.drawable.icon_info))
+                   .setTitle(appName)
+                   .setNeutralButton(ok , new DialogInterface.OnClickListener() {
+                       public void onClick(DialogInterface dialog, int id) {
+                            dialog.cancel();
+                       }
+                   });
+            dialog = builder.create();
+            break;
+        default:
+            dialog = null;
+        }
+
+        return dialog;
+    }
+    
     private OnClickListener startButtonListener = new OnClickListener() {
         public void onClick(View v) {
             Intent i = new Intent(WelcomeActivity.this, ControlActivity.class);
@@ -68,5 +107,11 @@ public class WelcomeActivity extends Activity
             Intent i = new Intent(WelcomeActivity.this, SettingsActivity.class);
             startActivity(i);
         }
+    };
+    
+    private OnClickListener infoButtonListener = new OnClickListener() {
+    	public void onClick(View v){
+    		showDialog(WelcomeActivity.ID_DIALOG_INFO);
+    	}
     };
 }
