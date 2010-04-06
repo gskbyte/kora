6af09397ed5be6b6add6d55aea7801e308fb6500
Jose Alcalá Correa <jose.alcala.correa@gmail.com>
2010-04-06 21:24:45 +0000

    KoraButton refactorizado.

--- src/org/gskbyte/kora/customViews/koraButton/KoraButton.java
+++ src/org/gskbyte/kora/customViews/koraButton/KoraButton.java
@@ -9,53 +9,123 @@ import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.RectF;
 import android.graphics.Typeface;
+import android.graphics.Paint.Align;
 import android.graphics.drawable.Drawable;
 import android.view.MotionEvent;
 import android.view.View;
 
 public class KoraButton extends View
 {
-    private static final String TAG = "KoraButton";
+    public class Attributes
+    {
+        public static final int HORIZONTAL = 1,
+                                VERTICAL = 2,
+                                BOTH = 3; // HORIZONTAL | VERTICAL
+        
+        public static final float TEXT_BIG = 35,
+                                  TEXT_MEDIUM = 25,
+                                  TEXT_SMALL = 15;
+        
+        public static final int BG_NORMAL_COLOR = Color.WHITE,
+                                BG_FOCUSED_COLOR = Color.WHITE,
+                                BG_SELECTED_COLOR = 0xFF7FB6E8; // 127, 182, 232 rgb
+        
+        public static final int BORDER_NORMAL_COLOR = 0xFF348BD4, // 52 139 212 rgb
+                                BORDER_FOCUSED_COLOR = 0xFF7FB6E8,
+                                BORDER_SELECTED_COLOR = 0xFF7FB6E8;
+        
+        // Orientación
+        public int allowed_orientations = BOTH;
+        public float max_proportion = 2.25f; // máxima relación v/h para poner horizontal
+        
+        // Texto
+        public boolean showText = true;
+        public float textMaxSize = TEXT_MEDIUM;
+        public Typeface typeface = Typeface.DEFAULT;
+        public int textColor = Color.BLACK;
+        
+        // Fondo
+        public int[] backgroundColors = { BG_NORMAL_COLOR,
+                                          BG_FOCUSED_COLOR,
+                                          BG_SELECTED_COLOR };
+        
+        // Borde
+        public int[] borderColors ={ BG_NORMAL_COLOR,
+                                     BORDER_FOCUSED_COLOR,
+                                     BORDER_SELECTED_COLOR };
+    }
+    
+    public static final String TAG = "KoraButton";
 
     public static final int STATE_NORMAL = 0,
                             STATE_FOCUSED = 1,
                             STATE_SELECTED = 2;
+    
+    // Superficie para dibujar
+    protected static Paint sPaint = new Paint();
+    protected static float sMinTextSize = -1;
+    
+    // Propiedades generales del botón
+    private String mText;
+    private Bitmap mIcon;
+    protected int mState;
+    protected Attributes mAttrs;
+    protected int mOrientation;
 
+    // Variables utilizadas para dibujar
+    protected int mWidth, mHeight;
+    protected int mBorderSize;
+    protected int mIconX, mIconY, mIconWidth, mIconHeight;
+    protected int mTextX, mTextY;
+    protected float mTextSize;
+    
+    protected OnClickListener mClickListener;
+    
     public KoraButton(Context context, String text, Drawable icon,
             Attributes attr)
     {
         super(context);
-        // this(context, text, icon.d)
     }
-
+    
+    public KoraButton(Context context, String text, int iconId)
+    {
+        this(context, text, BitmapFactory.decodeResource(
+                context.getResources(), iconId));
+    }
+    
     public KoraButton(Context context, String text, int iconId, Attributes attr)
     {
         this(context, text, BitmapFactory.decodeResource(
                 context.getResources(), iconId), attr);
     }
 
+    public KoraButton(Context context, String text, Bitmap icon)
+    {
+        this(context, text, icon, null);
+    }
+    
     public KoraButton(Context context, String text, Bitmap icon, Attributes attr)
     {
         super(context);
-
-        if (text != null)
-            mText = text;
-        else
-            mText = text;
-
+        
+        mText = text;
         mIcon = icon;
-
-        if (attr != null)
-            mAttributes = attr;
-        else
-            mAttributes = new Attributes();
-
         mState = STATE_NORMAL;
-
+        
+        if(attr!=null)
+            mAttrs = attr;
+        else
+            mAttrs = new Attributes();
+        
         setFocusable(true);
         setClickable(true);
     }
-
+    
+    public static void resetMinTextSize()
+    {
+        sMinTextSize = -1;
+    }
+    
     protected void onFocusChanged(boolean gainFocus, int direction,
             Rect previouslyFocusedRect)
     {
@@ -70,156 +140,171 @@ public class KoraButton extends View
 
     protected void onDraw(Canvas canvas)
     {
-        // Pintar borde
-        if (mAttributes.showBorder) {
-            Paint borderPaint = new Paint();
-            borderPaint.setAntiAlias(true);
-            borderPaint.setColor(mAttributes.borderColors[mState]);
-            RectF borderRect = new RectF(0, 0, mWidth, mHeight);
-            canvas.drawRoundRect(borderRect, 10, 10, borderPaint);
-        }
-
-        // Pintar fondo
-        final Paint backPaint = new Paint();
-        backPaint.setAntiAlias(true);
-        backPaint.setColor(mAttributes.backgroundColors[mState]);
-        RectF backRect;
-        if (mAttributes.showBorder)
-            backRect = new RectF(mBackX, mBackY, mBackXMax, mBackYMax);
-        else
-            backRect = new RectF(0, 0, mWidth, mHeight);
-        canvas.drawRoundRect(backRect, 6, 6, backPaint);
+        sPaint.setAntiAlias(true);
+        
+        // Borde
+        sPaint.setColor(mAttrs.borderColors[mState]);
+        RectF borderRect = new RectF(0, 0, mWidth, mHeight);
+        canvas.drawRoundRect(borderRect, 5, 5, sPaint);
+        
+        // Fondo
+        sPaint.setColor(mAttrs.backgroundColors[mState]);
+        RectF backRect  = new RectF(mBorderSize, mBorderSize,
+                             mWidth-mBorderSize, mHeight-mBorderSize);
+        canvas.drawRoundRect(backRect, 6, 6, sPaint);
 
         // Icono
         Bitmap b = Bitmap.createScaledBitmap(mIcon, mIconWidth, mIconHeight,
-                true);
+                                             true);
         canvas.drawBitmap(b, mIconX, mIconY, null);
 
         // Texto
-        if (mAttributes.showText) {
-            Paint textPaint = new Paint();
-            textPaint.setAntiAlias(true);
-            textPaint.setColor(Color.BLACK);
-            textPaint.setTypeface(mAttributes.typeface);
-            textPaint.setTextSize(80);
-            
-            int maxWidth = mWidth / 2;
-            
-            boolean ok = false;
-            Rect    bounds = new Rect();
-            float size = 80;
-            
-            if(mAttributes.orientation == Attributes.VERTICAL){
-                textPaint.setTextAlign(Paint.Align.CENTER);
-                maxWidth = mWidth;
-            }
-            while(!ok){
-                textPaint.getTextBounds(mText, 0, mText.length(), bounds);
-                if(bounds.width()<maxWidth){
-                    ok = true;
-                } else {
-                    size -= 0.25;
-                    textPaint.setTextSize(size);
-                }
-            }
-            
-            canvas.drawText(mText, mTextX, mTextY, textPaint);
-            /*
-            Paint textPaint = new Paint();
-            textPaint.setAntiAlias(true);
-            textPaint.setColor(Color.BLACK);
-            textPaint.setTypeface(mAttributes.typeface);
-            if(mAttributes.orientation == Attributes.VERTICAL){
-                textPaint.setTextAlign(Paint.Align.CENTER);
-                textPaint.setTextSize(mAttributes.textScale*(Math.min(mWidth, mHeight)>>2));
+        if(mAttrs.showText) {
+            sPaint.setColor(mAttrs.textColor);
+            sPaint.setTextSize(sMinTextSize);
+            if(mOrientation == Attributes.VERTICAL) {
+                sPaint.setTextAlign(Align.CENTER);
             } else {
-                textPaint.setTextSize(mAttributes.textScale*Math.min(mWidth, mHeight)/3);
+                sPaint.setTextAlign(Align.LEFT);
             }
-            canvas.drawText(mText, mTextX, mTextY, textPaint);
-            */
+            canvas.drawText(mText, mTextX, mTextY, sPaint);
         }
     }
-
+    
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec)
     {
         mWidth = MeasureSpec.getSize(widthMeasureSpec);
         mHeight = MeasureSpec.getSize(heightMeasureSpec);
         
-        if(mWidth>2.25*mHeight)
-            mAttributes.orientation = Attributes.HORIZONTAL;
-        else
-            mAttributes.orientation = Attributes.VERTICAL;
+        switch(mAttrs.allowed_orientations){
+        case Attributes.VERTICAL:
+            mOrientation = Attributes.VERTICAL;
+            break;
+        case Attributes.HORIZONTAL:
+            mOrientation = Attributes.HORIZONTAL;
+            break;
+        default:
+        case Attributes.BOTH:
+            if(mWidth>mAttrs.max_proportion*mHeight)
+                mOrientation = Attributes.HORIZONTAL;
+            else
+                mOrientation = Attributes.VERTICAL;
+            break;
+        }
 
-        calculateCoordinates();
-
+        calculateBorder();
+        calculateIconBounds();
+        if(mAttrs.showText) {
+            calculateTextBounds();
+        }
+            
         setMeasuredDimension(mWidth, mHeight);
     }
-
-    protected void calculateCoordinates()
+    
+    protected void calculateBorder()
     {
-        int iconX=0, iconY=0, iconW=0, iconH=0;
-        int minSize = Math.min(mWidth, mHeight);
-        
-        // El ancho del borde se toma relativo a la anchura del widget
-        if (mAttributes.showBorder) {
-            mBackX = mBackY = (int)((minSize >> 4)*mAttributes.borderScale);
-            mBackXMax = mWidth - mBackX;
-            mBackYMax = mHeight - mBackX;
-        } else {
-            mBackX = mBackY = 0; // width/16
-            mBackXMax = mWidth;
-            mBackYMax = mHeight;
-        }
-        
-        iconX = iconY = (minSize >> 4) + mBackX; // width*2/16
-        if (mAttributes.orientation == Attributes.HORIZONTAL) {
-            iconH = mHeight - (iconY << 1);
-            if (mAttributes.showText) {
-                iconW = (mWidth>>1) - (iconX << 1);
-                mTextX = mWidth>>1;
-                mTextY = (mHeight>>1) + (int)mAttributes.textScale*8;
-            } else {
-                iconW = mWidth - (iconX << 1);
-            }
-        } else {
-            iconW = mWidth - (iconX << 1);
-            if (mAttributes.showText) {
-                iconH = mHeight - (iconY << 1) - (int)(mAttributes.textScale*(minSize>>2));
-                mTextX = mWidth >>1;
-                mTextY = mHeight * 14 / 16;
-            } else {
-                iconH = mHeight - (iconY << 1);
-            }
-        }
-        
-        // Establecer tamaño mínimo para iconos
-        if(iconW < 10)
-            iconW = 10;
-
-        if(iconH < 10)
-            iconH = 10;
-
-        // Escalar icono
-        // para las proporciones, multplico y divido por 1024
-        // en vez de usar flotantes, es algo más rápido
-        int w = mIcon.getWidth(),
-            h = mIcon.getHeight();
-        int rw = (iconW<<10) / w,
-            rh = (iconH<<10) / h;
-        
-        int res = (rw < rh) ? rw : rh;
-        
-        mIconWidth = (res*w)>>10;
-        mIconHeight = (res*h)>>10;
-        mIconX = iconX + (Math.abs(mIconWidth - iconW)>>1);
-        mIconY = iconY + (Math.abs(mIconHeight - iconH)>>1);
+        // 1/16 del mínimo
+        mBorderSize = Math.min(mWidth, mHeight)>>4;
     }
     
-    public void setOnClickListener(OnClickListener newListener)
+    protected void calculateIconBounds()
     {
-        clickListener = newListener;
+        /* Si no hay texto, el icono ocupa todo el botón
+         * 
+         * Si hay:
+         * 
+         * - Si la orientación es horizontal el icono ocupa 1/4 del total
+         * - Si es vertical, 3/4.
+         * 
+         * En cualquier caso se resta el doble de los bordes, o se escala al menor 
+         * lado si es menor. La imagen se escala manteniendo proporciones.
+         * 
+         * No se permiten valores menores o iguales que 0.
+         */
+        int maxSize,
+            maxWidth,
+            maxHeight;
+        int border = mBorderSize+2,
+            doubleBorder = (mBorderSize+2)<<1;
+        
+        if(mAttrs.showText) {
+            if(mOrientation == Attributes.VERTICAL) {
+                maxWidth = mWidth - doubleBorder;
+                maxHeight = ((mHeight-doubleBorder)*3)/4 ;
+            } else {
+                maxWidth = (mWidth-doubleBorder)/4;
+                maxHeight = mHeight - doubleBorder;
+            }
+        } else {
+            maxWidth = mWidth - doubleBorder;
+            maxHeight = mHeight - doubleBorder;
+        }
+        maxSize = Math.min( maxWidth,
+                                maxHeight );
+        
+        if(maxSize < 1)
+            maxSize = 1;
+        // Multiplico y divido por 1024 para hallar las proporciones, es más rápido
+        // que usar floats
+        int iw = mIcon.getWidth(),
+            ih = mIcon.getHeight();
+        int rw = (maxSize<<10) / iw,
+            rh = (maxSize<<10) / ih;
+        int res = Math.min(rw, rh);
+        mIconWidth = (res*iw)>>10;
+        mIconHeight = (res*ih)>>10;
+        
+        mIconX = border + ((maxWidth-mIconWidth)>>1) ;
+        mIconY = border + ((maxHeight-mIconHeight)>>1) ;
     }
-
+    
+    protected void calculateTextBounds()
+    {
+        //sPaint.setAntiAlias(false);
+        sPaint.setTypeface(mAttrs.typeface);
+        
+        int maxWidth, maxHeight;
+        int border = mBorderSize*2;
+        
+        if(mOrientation == Attributes.VERTICAL) {
+            maxWidth = mWidth - border;
+            maxHeight = (mHeight-border)/4;
+            mTextSize = mAttrs.textMaxSize;
+        } else {
+            maxWidth = ((mWidth-border)*3)/4;
+            maxHeight = mHeight - border;
+            mTextSize = mAttrs.textMaxSize*1.5f;
+        }
+        
+        boolean ok = false;
+        Rect bounds = new Rect();
+        sPaint.setTextSize(mAttrs.textMaxSize);
+        while(!ok){
+            sPaint.getTextBounds(mText, 0, mText.length(), bounds);
+            if(bounds.width()<=maxWidth &&
+               (bounds.height()-bounds.bottom)<=maxHeight){
+                ok = true;
+                if(mTextSize < sMinTextSize || sMinTextSize == -1)
+                    sMinTextSize = mTextSize;
+            } else {
+                mTextSize -= 1.0f;
+                sPaint.setTextSize(mTextSize);
+            }
+        }
+        
+        if(mOrientation == Attributes.VERTICAL) {
+            mTextX = mWidth>>1;
+            mTextY = mHeight - maxHeight/2;
+        } else {
+            mTextX = mWidth/4 + mBorderSize;
+            mTextY = (mHeight+bounds.height())>>1;
+        }
+    }
+    
+    public void setOnClickListener(OnClickListener listener)
+    {
+        mClickListener = listener;
+    }
     
     @Override
     public boolean onTouchEvent(MotionEvent event)
@@ -233,14 +318,13 @@ public class KoraButton extends View
                 invalidate();
                 break;
             case MotionEvent.ACTION_UP:
-                @SuppressWarnings("unused")
                 int x = (int) event.getX(),
                     y = (int) event.getY();
                 
                 if(x<mWidth && y<mHeight && mState==STATE_FOCUSED){
                     mState = STATE_SELECTED;
-                    if(clickListener!=null)
-                        clickListener.onClick(this);
+                    if(mClickListener!=null)
+                        mClickListener.onClick(this);
                 } else {
                     mState = STATE_NORMAL;
                 }
@@ -275,62 +359,12 @@ public class KoraButton extends View
 
     public Attributes getAttributes()
     {
-        return mAttributes;
+        return mAttrs;
     }
 
     public void setAttributes(Attributes attr)
     {
-        mAttributes = attr;
-        calculateCoordinates();
+        mAttrs = attr;
         invalidate();
     }
-
-    public class Attributes
-    {
-        public static final int HORIZONTAL = 0, VERTICAL = 1;
-
-        public int orientation = VERTICAL;
-
-        public boolean showText = true;
-        public float textScale = (float) 1.0;
-        public Typeface typeface = Typeface.DEFAULT;
-
-        public boolean showBorder = true;
-        public float borderScale = (float) 1.0;
-        public int[] borderColors = { Color.rgb(52,139,212), Color.rgb(255, 165, 0), Color.rgb(255, 165, 0) };
-
-        public int[] backgroundColors = { Color.WHITE, Color.WHITE, Color.rgb(255, 165, 0) };
-
-        public Attributes()
-        {
-        }
-
-        public Attributes(int orientation, boolean showText,
-                boolean showBorder, int[] borderColors, int[] backgroundColors,
-                Typeface typeface)
-        {
-            this.orientation = orientation;
-            this.showText = showText;
-            this.showBorder = showBorder;
-
-            if (borderColors.length == 3)
-                this.borderColors = borderColors;
-            if (backgroundColors.length == 3)
-                this.backgroundColors = backgroundColors;
-        }
-    }
-    
-    // Propiedades generales del botón
-    private String mText;
-    private Bitmap mIcon;
-    private int mState;
-    private Attributes mAttributes;
-
-    // Variables utilizadas para dibujar
-    private int mWidth, mHeight;
-    private int mBackX, mBackY, mBackXMax, mBackYMax;
-    private int mIconX, mIconY, mIconWidth, mIconHeight;
-    private int mTextX, mTextY;
-    
-    private OnClickListener clickListener;
 }
--- src/org/gskbyte/kora/handlingActivities/DeviceSelectionActivity.java
+++ src/org/gskbyte/kora/handlingActivities/DeviceSelectionActivity.java
@@ -1,15 +1,14 @@
 package org.gskbyte.kora.handlingActivities;
 
 import org.gskbyte.kora.R;
-import org.gskbyte.kora.R.drawable;
-import org.gskbyte.kora.R.id;
-import org.gskbyte.kora.R.layout;
 import org.gskbyte.kora.customViews.GridLayout;
 import org.gskbyte.kora.customViews.koraButton.KoraButton;
+import org.gskbyte.kora.customViews.koraButton.KoraButton.Attributes;
 
 import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.AttributeSet;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -31,22 +30,21 @@ public class DeviceSelectionActivity extends Activity
         mGrid.setClipToPadding(false);
         mGrid.setDimensions(3, 2);
         
-        
-        KoraButton w = new KoraButton(this, "Heater", R.drawable.icon_device_heater_128, null),
-                   w2 = new KoraButton(this, "Light", R.drawable.icon_device_light_128, null),
-                   w3 = new KoraButton(this, "Music", R.drawable.icon_device_music_128, null),
-                   w4 = new KoraButton(this, "Sunblind", R.drawable.icon_device_sunblind_128, null),
-                   w5 = new KoraButton(this, "Light", R.drawable.icon_device_light_128, null);
+        KoraButton.resetMinTextSize();
+        KoraButton w = new KoraButton(this, "Heater", R.drawable.icon_device_heater_128),
+                   w2 = new KoraButton(this, "Light", R.drawable.icon_device_light_128),
+                   w3 = new KoraButton(this, "Music", R.drawable.icon_device_music_128),
+                   w4 = new KoraButton(this, "Sunblind", R.drawable.icon_device_sunblind_128),
+                   w5 = new KoraButton(this, "Light", R.drawable.icon_device_light_128);
         
         KoraButton.Attributes attr;
         
         attr = w.getAttributes();
-        attr.borderScale = (float) 1.2;
         attr.backgroundColors[KoraButton.STATE_FOCUSED] = Color.rgb(15, 100, 15);
         w.setAttributes(attr);
         
         attr = w2.getAttributes();
-        attr.textScale = (float) 1.5;
+        //attr.textMaxSize = Attributes.TEXT_BIG;
         attr.backgroundColors[KoraButton.STATE_FOCUSED] = Color.rgb(147, 166, 132);
         w2.setAttributes(attr);
         
